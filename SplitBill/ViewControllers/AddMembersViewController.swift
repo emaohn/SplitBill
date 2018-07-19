bcb00cc83042d92b79b2e8b9e3d28b8bff92a85c
lekhaadari <lekha_adari@umail.ucsb.edu>
2018-07-19 10:28:34 -0700

    minimal changes

--- SplitBill/ViewControllers/AddMembersViewController.swift
+++ SplitBill/ViewControllers/AddMembersViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 class AddMembersViewController: UITableViewController {
     var members = [Person]()
     var items = [Item]()
+    var personName: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +30,37 @@ class AddMembersViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 1
         return members.count
+        
     }
+    
+    func addMembersButtonPressed(_ sender: Any) {
+        // Create the alert controller
+        let alertController = UIAlertController(title: "Person's Name", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter Person's Name"
+        }
+        // Create the actions
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            if let personNameTextField = alertController.textFields![0] as UITextField? {
+                self.personName = personNameTextField.text!
+                print(self.personName)
+            } else {return}
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+            UIAlertAction in
+            NSLog("Cancel Pressed")
+        }
+        
+        // Add the actions
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+    }
+
     
     @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
         for member in members {
@@ -50,8 +81,11 @@ class AddMembersViewController: UITableViewController {
         case "backButton":
             print("back button pressed")
         case "addMember":
+            addMembersButtonPressed(self)
             let vc = segue.destination as? SelectItemsViewController
             vc?.items = self.items
+            vc?.member.name = self.personName
+
         default:
             print("i dont recognize this")
         }
